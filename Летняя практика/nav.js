f5462f99816a56dc91f3170bb7a165deaae58e98
Hazelgr0ver <rublev.ea@mail.ru>
2025-08-01 19:03:12 +0700

    Delete Летняя практика directory

--- Летняя практика/nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navLinks = document.querySelector('.nav-links');
-    if (navToggle && navLinks) {
-        navToggle.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-        });
-    }
-});
